f5cc487bf9799730e276af1917a16c3970add580
Kenneth Love <kenneth.love81@gmail.com>
2017-12-11 14:59:54 -0500

    created js file

---
+++ script.js
@@ -0,0 +1,11 @@
+// create array of words
+const word = ['the story of oj','bodak yellow', 'thats what i like', 'despacito', 'wild thoughts', 'hotline bling', 'humble'];
+// choose word randomly
+let randNum = Math.floor(Math.random() * word.length);
+let choosenWord = word[randNum];
+console.log(choosenWord);
+// create underscores based on length of word.
+// get users guess
+// check if guess is right
+// if right push to correct array
+// if wrong push to incorrect array
